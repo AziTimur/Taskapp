4eeef9c7416b039ff8540ea8eb8241a88fea15bd
AziTimur <azik00kgz@gmail.com>
2021-08-20 16:21:47 +0600

    Initial commit

---
+++ app/src/main/java/com/example/taskapp/ui/profile/ProfileFragment.java
@@ -0,0 +1,77 @@
+package com.example.taskapp.ui.profile;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.taskapp.R;
+
+import org.jetbrains.annotations.NotNull;
+
+import static android.app.Activity.RESULT_OK;
+
+
+public class ProfileFragment extends Fragment {
+
+    int SELECT_PICTURE = 2;
+    ImageView IVPreviewImage;
+    ImageView image;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        image = view.findViewById(R.id.IVPreviewImage);
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                choiser();
+            }
+
+
+        });
+
+    }
+
+    public void choiser(){
+        Intent intent = new Intent();
+        intent.setAction(android.content.Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, SELECT_PICTURE);
+    }
+    ;
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                Log.e("test2", " =" + selectedImageUri);
+                if (null != selectedImageUri) {
+                    image.setImageURI(selectedImageUri);
+                }
+
+            }
+        }
+
+}
+
